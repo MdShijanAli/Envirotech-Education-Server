fd6fa85f291c2f3dcc628018a37f8f0a45d1e2c3
FreelancerShijan <freelancershijan@gmail.com>
2022-10-25 04:16:21 +0600

    gitignore

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Courses API Running');
+})
+
+
+const courses = require('./data/courses.json');
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.listen(port, () => {
+    console.log('Courses Server Running on POrt', port)
+})
