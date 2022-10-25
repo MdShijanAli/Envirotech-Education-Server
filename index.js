f232371d5a0e84aedeef5ae4b73a067b4e233f32
FreelancerShijan <freelancershijan@gmail.com>
2022-10-26 02:06:08 +0600

    blog json add

--- index.js
+++ index.js
@@ -16,9 +16,28 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
+
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedCourse = courses.find(c => c.id === id);
+    res.send(selectedCourse)
+
+})
+
+const homeCourse = require('./data/home-course.json');
+
+app.get('/home-course', (req, res) => {
+    res.send(homeCourse);
+})
+
 const blog = require('./data/blog.json');
 app.get('/blog', (req, res) => {
     res.send(blog);
+})
+
+const homeBlog = require('./data/home-blog.json');
+app.get('/home-blog', (req, res) => {
+    res.send(homeBlog);
 })
 
 app.listen(port, () => {
